54580219c0089e5246ab9d5e57f0938127eba318
Garrett Thornburg <film42@gmail.com>
2017-09-01 13:24:51 -0400

    Add queue size and message dropped metrics to project

--- lib/harness/active_publisher.rb
+++ lib/harness/active_publisher.rb
@@ -1,5 +1,13 @@
 require "harness/active_publisher/version"
 
+::ActiveSupport::Notifications.subscribe "async_queue_size.active_publisher" do |async_queue_size|
+  ::Harness.counter "active_publisher.async_queue_size", async_queue_size
+end
+
+::ActiveSupport::Notifications.subscribe "message_dropped.active_publisher" do
+  ::Harness.increment "active_publisher.message_dropped"
+end
+
 ::ActiveSupport::Notifications.subscribe "wait_for_async_queue.active_publisher" do |*args|
   event = ::ActiveSupport::Notifications::Event.new(*args)
   ::Harness.timing "active_publisher.waiting_for_async_queue", event.duration
