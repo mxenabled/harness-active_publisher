cfe8f0cf58de7e7f8255cb2ae400aef552461d5b
Michael Ries <michael@riesd.com>
2017-10-06 07:41:27 -0600

    derp, used the wrong environment variable name

--- lib/harness/active_publisher.rb
+++ lib/harness/active_publisher.rb
@@ -3,11 +3,11 @@ require "harness/active_publisher/version"
 require "harness"
 require "active_support"
 
-DROPPED_METRIC    = ["active_publisher", ENV["service"], "message_dropped"].reject(&:nil?).join(".").freeze
-LATENCY_METRIC    = ["active_publisher", ENV["service"], "publish_latency"].reject(&:nil?).join(".").freeze
-PUBLISHED_METRIC  = ["active_publisher", ENV["service"], "messages_published"].reject(&:nil?).join(".").freeze
-QUEUE_SIZE_METRIC = ["active_publisher", ENV["service"], "async_queue_size"].reject(&:nil?).join(".").freeze
-WAIT_METRIC       = ["active_publisher", ENV["service"], "waiting_for_async_queue"].reject(&:nil?).join(".").freeze
+DROPPED_METRIC    = ["active_publisher", ENV["SERVICE_NAME"], "message_dropped"].reject(&:nil?).join(".").freeze
+LATENCY_METRIC    = ["active_publisher", ENV["SERVICE_NAME"], "publish_latency"].reject(&:nil?).join(".").freeze
+PUBLISHED_METRIC  = ["active_publisher", ENV["SERVICE_NAME"], "messages_published"].reject(&:nil?).join(".").freeze
+QUEUE_SIZE_METRIC = ["active_publisher", ENV["SERVICE_NAME"], "async_queue_size"].reject(&:nil?).join(".").freeze
+WAIT_METRIC       = ["active_publisher", ENV["SERVICE_NAME"], "waiting_for_async_queue"].reject(&:nil?).join(".").freeze
 
 ::ActiveSupport::Notifications.subscribe "async_queue_size.active_publisher" do |_, _, _, _, async_queue_size|
   ::Harness.gauge QUEUE_SIZE_METRIC, async_queue_size
