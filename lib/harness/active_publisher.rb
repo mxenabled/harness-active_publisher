8b112e13a8d1a65ab5ba932e4b55413bf236c1bc
Brandon Dewitt <brandonsdewitt@gmail.com>
2020-05-10 11:01:57 -0600

    capture redis_async_queue_size as well

--- lib/harness/active_publisher.rb
+++ lib/harness/active_publisher.rb
@@ -5,14 +5,19 @@ require "active_support"
 
 module Harness
   module ActivePublisher
-    DROPPED_METRIC    = ["active_publisher", ENV["SERVICE_NAME"], "message_dropped"].reject(&:nil?).join(".").freeze
-    LATENCY_METRIC    = ["active_publisher", ENV["SERVICE_NAME"], "publish_latency"].reject(&:nil?).join(".").freeze
-    PUBLISHED_METRIC  = ["active_publisher", ENV["SERVICE_NAME"], "messages_published"].reject(&:nil?).join(".").freeze
-    QUEUE_SIZE_METRIC = ["active_publisher", ENV["SERVICE_NAME"], "async_queue_size"].reject(&:nil?).join(".").freeze
-    WAIT_METRIC       = ["active_publisher", ENV["SERVICE_NAME"], "waiting_for_async_queue"].reject(&:nil?).join(".").freeze
+    DROPPED_METRIC          = ["active_publisher", ENV["SERVICE_NAME"], "message_dropped"].reject(&:nil?).join(".").freeze
+    LATENCY_METRIC          = ["active_publisher", ENV["SERVICE_NAME"], "publish_latency"].reject(&:nil?).join(".").freeze
+    PUBLISHED_METRIC        = ["active_publisher", ENV["SERVICE_NAME"], "messages_published"].reject(&:nil?).join(".").freeze
+    QUEUE_SIZE_METRIC       = ["active_publisher", ENV["SERVICE_NAME"], "async_queue_size"].reject(&:nil?).join(".").freeze
+    REDIS_QUEUE_SIZE_METRIC = ["active_publisher", ENV["SERVICE_NAME"], "redis_async_queue_size"].reject(&:nil?).join(".").freeze
+    WAIT_METRIC             = ["active_publisher", ENV["SERVICE_NAME"], "waiting_for_async_queue"].reject(&:nil?).join(".").freeze
 
     ::ActiveSupport::Notifications.subscribe "async_queue_size.active_publisher" do |_, _, _, _, async_queue_size|
       ::Harness.gauge QUEUE_SIZE_METRIC, async_queue_size
+    end
+
+    ::ActiveSupport::Notifications.subscribe "redis_async_queue_size.active_publisher" do |_, _, _, _, redis_async_queue_size|
+      ::Harness.gauge REDIS_QUEUE_SIZE_METRIC, redis_async_queue_size
     end
 
     ::ActiveSupport::Notifications.subscribe "message_dropped.active_publisher" do
